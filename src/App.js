8a6e747587c780672bf9d94732e5601c1bf63162
Matthew Holland <woodshoes@gmail.com>
2019-10-27 16:39:33 -0400

    i'm trying here

--- src/App.js
+++ src/App.js
@@ -1,66 +1,32 @@
-import React from "react";
-import { useEffect, useReducer } from "react"; // using hooks
-import { listTodos } from "./graphql/queries";
-import { onCreateTodo } from "./graphql/subscriptions";
-import API, { graphqlOperation } from "@aws-amplify/api";
-import PubSub from "@aws-amplify/pubsub";
-import { createTodo } from "./graphql/mutations";
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { withAuthenticator } from "aws-amplify-react";
+import Amplify, { Auth } from "aws-amplify";
+import aws_exports from "./aws-exports";
+Amplify.configure(aws_exports);
 
-import config from "./aws-exports";
-API.configure(config); // Configure Amplify
-PubSub.configure(config);
-
-const initialState = { todos: ["hello"] };
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "QUERY":
-      return { ...state, todos: action.todos };
-    case "SUBSCRIPTION":
-      return { ...state, todos: [...state.todos, action.todo] };
-    default:
-      return state;
-  }
-};
-
-function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  useEffect(() => {
-    getData();
-    const subscription = API.graphql(graphqlOperation(onCreateTodo)).subscribe({
-      next: eventData => {
-        const todo = eventData.value.data.onCreateTodo;
-        dispatch({ type: "SUBSCRIPTION", todo });
-      }
-    });
-    return () => subscription.unsubscribe();
-  }, []);
-
-  async function getData() {
-    const todoData = await API.graphql(graphqlOperation(listTodos));
-    dispatch({ type: "QUERY", todos: todoData.data.listTodos.items });
-  }
-
-  async function createNewTodo() {
-    const todo = { name: "Ay Dog", description: "Realtime and Offline" };
-    await API.graphql(graphqlOperation(createTodo, { input: todo }));
-  }
-
-  return (
-    <div>
+class App extends Component {
+  render() {
+    return (
       <div className="App">
-        <button onClick={createNewTodo}>Add Todo</button>
-      </div>
-      <div>
-        {state.todos.map((todo, i) => (
-          <p key={todo.id}>
-            {todo.name} : {todo.description}
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
           </p>
-        ))}
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
+export default withAuthenticator(App, true);
